a7c257fd6d323af91fe07471dc86835ecbe9d95a
manoel jailton <manoeljailton79@gmail.com>
2022-06-05 10:25:11 -0300

    jquery-2

--- js/custom.js
+++ js/custom.js
@@ -49,14 +49,160 @@ $(document).ready(function(){
 
      })
      
-     
+     $('.featured-item:nth(1)')
+     .hide(500, function(){
+        // este é o callback
+        console.log( $(this).find('h4').text() + ' esgotado')
+     })
+     .show(500, function(){
+        console.log( $(this).find('h4').text() + ' em estoque')
+     })
+
+
+  /*
+   * Animações
+   */
+  const duracao = 1000 // equivalenta a 1 segundo
+
+  $('.featured-item:nth(0)')
+     .hide(duracao)
+     .show(duracao)
+     .fadeOut(duracao)
+     .fadeIn(duracao)
+     .toggle(duracao)
+     .toggle(duracao)
+
+  $('#form-submit').on('click', function(e){
+
+     e.preventDefault()
+
+     if( $('#email').val() != '' ){
+
+        $('#email').animate({
+           opacity: "toggle",
+           top: "-50"
+        }, 500, function(){
+           console.log($(this).val())
+        })
+
+     }
+
+
+  });
+ 
      
      
 
 
+
+
+
+
+
+
+
+/*
+* eventos .nav-modal-open
+*/
+
+$('.nav-modal-open').on('click', function(e){
+   
+   e.preventDefault();
+    
+   let elem = $(this).attr('rel')
+
+   $('.modal-body').html($('#'+elem).html())
+
+   $('.modal-header h5.modal-title').html($(this).text())
+
+   let myModal = new bootstrap.Modal($('#modelId'))
+
+   myModal.show()
+})
+
+ function validate( elem ){
+      if( elem.val() == '') {
+
+         console.log('o campo de '+ elem.attr('nome') +  'é obrigatório')
+
+         elem.parent().find('.text-muted').show()
+   
+         elem.addClass('invalid')
+   
+         return false
+
+   } else {
+      elem.parent().find(' text-muted').hide()
+      elem.removeClass('invalid')
+   }
+}
+
+$('.body').on('submit', '.modal-body .form', function(e){
+
+   e.preventDefault()
+
+   const inputName = $('#nome')
+   const inputEmail = $('#email')
+
+   validate(inputName) 
+   validate(inputEmail) 
+
+   if(inputEmail.hasClass('invalid')  || inputName.hasClass('invalid')) {
+         console.log('verificar campos obrigatórios')
+         return false
+       
+   } else { 
+      $(this).submit()
+      
+   }
+
+})  
+
+$('body').on('blur' , '#nome' , function(){
+   validate($(this))
+
+})
+
+$('body').on('blur' , '#email' , function(){
+   validate($(this))
+})
+
+
+
+$('body').on('blur' , '#date' , function(){
+   validate($(this))
+   $(this).mask('00/00/0000');
+})
+
+$('body').on('blur' , '#cep' , function(){
+   validate($(this))
+   $(this).mask('00000-000');
+})
+
+$('body').on('blur' , '#cpf' , function(){
+   validate($(this))
+   $(this).mask('000.000.000-00');
+})
+
+$('body').on('blur' , '#phone' , function(){
+   validate($(this))
+   $(this).mask('(00)0 0000-0000')
+})
 
 
 
 
 
 })
+
+
+
+
+     
+
+
+  
+
+
+
+
